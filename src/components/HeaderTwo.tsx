13dcb96abcac32ac7110bb883d55cf202c4d5487
Tom Gatlin <tomgatlin@Mac-6358.cable.rcn.com>
2025-11-16 12:45:46 -0500

    add conditional text color based on page

--- src/components/HeaderTwo.tsx
+++ src/components/HeaderTwo.tsx
@@ -1,8 +1,12 @@
-import { Link } from '@tanstack/react-router'
+import { Link, useLocation} from '@tanstack/react-router'
 
 const HeaderTwo = () => {
+    const location = useLocation()
+    const homeMatch = location.pathname === '/';
+    const textColor = homeMatch ? 'text-white' : 'text-black'
+
   return (
-    <div className='p-4 flex items-center justify-end text-black'>
+    <div className={`fixed top-0 left-0 w-full p-4 flex items-center justify-end text-black bg-transparent z-50 ${textColor}`}>
         <Link to="/" className="mx-4 hover:underline">Home</Link>
         <Link to="/about" className="mx-4 hover:underline">About</Link>
         <Link to="/tom" className="mx-4 hover:underline">Tom</Link>
